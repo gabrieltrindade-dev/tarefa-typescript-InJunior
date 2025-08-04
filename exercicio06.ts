d052255e29c655f52bab5b98cb19d37894d61be6
Gabriel Trindade <gabrielt20514@gmail.com>
2025-08-04 19:49:36 -0300

    style: comentario resposta

--- exercicio06.ts
+++ exercicio06.ts
@@ -4,8 +4,8 @@ function retornarElemento<T>(array: T[], index: number): T {
 
 const numeros = [10, 20, 30];
 const elementoNumero = retornarElemento(numeros, 1);
-console.log(elementoNumero);
+console.log(elementoNumero); //20
 
 const letras = ["a", "b", "c"];
 const elementoString = retornarElemento(letras, 0);
-console.log(elementoString);
+console.log(elementoString); //a
